4b89c62eaea8cbd8923f2b429be30013d22fa37f
Marcos Saavedra <marcsaav96@gmail.com>
2020-11-05 15:34:20 -0500

    Built MVP, as well as added a remove item functionality

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+
+// Context
+import { CartContext } from './../contexts/CartContext'
 
 // Components
 import Item from './ShoppingCartItem';
 
-const ShoppingCart = props => {
+const ShoppingCart = () => {
+	const [cart, setCart] = useContext(CartContext)
+
 	const getCartTotal = () => {
-		return props.cart.reduce((acc, value) => {
+		return cart.reduce((acc, value) => {
 			return acc + value.price;
 		}, 0).toFixed(2);
 	};
 
 	return (
 		<div className="shopping-cart">
-			{props.cart.map(item => (
-				<Item key={item.id} {...item} />
+			{cart.map(item => (
+				<Item key={item.id} {...item} cart={cart} setCart={setCart} />
 			))}
 
 			<div className="shopping-cart__checkout">
--- src/components/ShoppingCartItem.js
+++ src/components/ShoppingCartItem.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
 const Item = props => {
+	const {cart, setCart} = props
+
+	const removeItem = e => {
+		setCart(cart.filter((item) => {
+			if(item.id !== props.id){
+				return item
+			} else {
+				return null
+			}
+		}))
+	}
+
 	return (
 		<div className="shopping-cart_item">
 			<img src={props.image} alt={`${props.title} book`} />
@@ -9,7 +21,7 @@ const Item = props => {
 			<div>
 				<h1>{props.title}</h1>
 				<p>$ {props.price}</p>
-				<button>Remove from cart</button>
+				<button onClick={removeItem}>Remove from cart</button>
 			</div>
 		</div>
 	);
